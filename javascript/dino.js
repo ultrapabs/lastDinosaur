bff8555e2f3ccb581c625c62a65999dfa1d732a3
App Academy Student <appacademy@Apps-Mac-mini-16.local>
2015-03-23 18:20:50 -0400

    adds initial game board display and keypress logic

--- javascript/dino.js
+++ javascript/dino.js
@@ -3,24 +3,37 @@
     window.LastDinosaur = {};
   }
 
-  var BOARD_WIDTH = 30;
-  var BOARD_HEIGHT = 10
+  var BOARD_WIDTH = 20;
+  var BOARD_HEIGHT = 10;
+
+  var Dino = LastDinosaur.Dino = function (board) {
+
+  };
 
   var Board = LastDinosaur.Board = function () {
-    this.board = [];
-    this.dino = new Dino();
+    this.earth = Board.freshEarth();
+    this.dino = new LastDinosaur.Dino(this);
+  };
+
+  Board.freshEarth = function () {
+    var earth = [];
 
     for (var i = 0; i < BOARD_HEIGHT; i++) {
-      this.board.push([]);
+      earth.push([]);
       for (var j = 0; j < BOARD_WIDTH; j++) {
-        this.board[i].push(0);
+        if (i > BOARD_HEIGHT - 3) {
+          earth[i].push(1);
+        } else {
+          earth[i].push(0);
+        }
       }
     }
+
+    return earth;
   };
 
   Board.prototype.render = function () {
-
-
+    var earth = freshEarth();
   };
 
 })();
--- javascript/dino_view.js
+++ javascript/dino_view.js
@@ -3,15 +3,46 @@
     window.LastDinosaur = {};
   }
 
-  var View = LastDinosaur = function ($el) {
+  var View = LastDinosaur.View = function ($el) {
     this.$el = $el;
 
     this.board = new LastDinosaur.Board();
+    this.renderEarth(this.board);
 
+    this.intervalId = window.setInterval(this.step.bind(this), 100);
+
+    $(window).on("keydown", this.moveDino.bind(this));
   };
 
+  View.prototype.moveDino = function (event) {
+    if (event.keyCode === 37) {
+      console.log("left!");
+    } else if (event.keyCode === 39) {
+      console.log("right!");
+    }
+  };
 
+  View.prototype.renderEarth = function (board) {
+    var html = '';
 
+    for (var i = 0; i < board.earth.length; i++) {
+      html += '<ul class="group">';
+      for (var j = 0; j < board.earth[0].length; j++) {
+        if (board.earth[i][j] === 1) {
+          html += '<li class="earth"></li>';
+        } else if (board.earth[i][j] === 2) {
+          html += '<li class="dino"></li>';
+        } else {
+          html += '<li></li>';
+        }
+      }
+      html += '</ul>';
+    }
+    this.$el.html(html);
+  };
 
+  View.prototype.step = function () {
+
+  };
 
 })();
